2915c8c23774ba451a3838eb94305cb880a7747f
yeonoo <ywoo818@gmail.com>
2015-04-17 02:53:59 +0900

    userDao_refactoring1
    Connection을 얻어오는 부분 중복 - 리팩토링

--- usermanagement/src/net/daum/UserDao.java
+++ usermanagement/src/net/daum/UserDao.java
@@ -8,8 +8,7 @@ import java.sql.SQLException;
 
 public class UserDao {
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.143/Spring?characterEncoding=utf-8", "root", "Starter");
+		Connection connection = getConnection();
 		
 		PreparedStatement preparedStatement = connection.prepareStatement(
 				"select * from userinfo where id = ?");
@@ -31,8 +30,7 @@ public class UserDao {
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.143/Spring?characterEncoding=utf-8", "root", "Starter");
+		Connection connection = getConnection();
 		
 		PreparedStatement preparedStatement = connection.prepareStatement(
 				"insert into userinfo(id, name, password) values(?, ?, ?)");
@@ -44,5 +42,12 @@ public class UserDao {
 		
 		preparedStatement.close();
 		connection.close();
+	}
+	
+	private Connection getConnection() throws ClassNotFoundException,
+	SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.143/Spring?characterEncoding=utf-8", "root", "Starter");
+		return connection;
 	}
 }
