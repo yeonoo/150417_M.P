d6adb8ab3e0c747003719d5dc906efe30037da1a
yeonoo <ywoo818@gmail.com>
2015-04-17 02:51:17 +0900

    주석삭제

--- usermanagement/src/net/daum/UserDao.java
+++ usermanagement/src/net/daum/UserDao.java
@@ -8,33 +8,21 @@ import java.sql.SQLException;
 
 public class UserDao {
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		// 사용자는 어디에 저장되어 있는 걸까?
-		// DB를 사용해야지
-		// 어떤 DB?
-		// Mysql을 사용하자!
-		
-		// 개략적인 순서는 다음과 같이!
-		
-		// Connection을 맺고
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.143/Spring?characterEncoding=utf-8", "root", "Starter");
 		
-		// 쿼리를 만들고
 		PreparedStatement preparedStatement = connection.prepareStatement(
 				"select * from userinfo where id = ?");
 		preparedStatement.setString(1, id);
 		
-		// 실행시키고
 		ResultSet resultSet = preparedStatement.executeQuery();
 		resultSet.next();
 		
-		// 결과를 User에 잘 매핑하고
 		User user = new User();
 		user.setId(resultSet.getString("id"));
 		user.setName(resultSet.getString("name"));
 		user.setPassword(resultSet.getString("password"));
 		
-		// 자원을 해지하기
 		resultSet.close();
 		preparedStatement.close();
 		connection.close();
@@ -43,26 +31,15 @@ public class UserDao {
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		// 사용자는 어디에 저장되어 있는 걸까?
-		// DB를 사용해야지
-		// 어떤 DB?
-		// Mysql을 사용하자!
-		
-		// 개략적인 순서는 다음과 같이!
-		
-		// Connection을 맺고
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.143/Spring?characterEncoding=utf-8", "root", "Starter");
 		
-		// 쿼리를 만들고
 		PreparedStatement preparedStatement = connection.prepareStatement(
 				"insert into userinfo(id, name, password) values(?, ?, ?)");
 		preparedStatement.setString(1, user.getId());
 		preparedStatement.setString(2, user.getName());
 		preparedStatement.setString(3, user.getPassword ());
 		
-		// 실행시키고
 		preparedStatement.executeUpdate();
 		
 		preparedStatement.close();
